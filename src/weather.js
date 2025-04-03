7d0438bd867c36166ac9643a705d73e3ca3d3224
Owolabi <owolabi1738@gmail.com>
2025-04-03 08:45:32 +0100

    Referenced relevant elements in the JS file

--- src/weather.js
+++ src/weather.js
@@ -1,1 +1,11 @@
-export const greeting = "Hello, Odinite!";
+function weatherApp() {
+    
+    const searchInput = document.querySelector('#search-input');
+    const searchButton = document.querySelector('#search-button');
+    const cityName = document.querySelector('#city-name');
+    const weatherImg = document.querySelector('#weather-img');
+    const temperature = document.querySelector('#temp');
+    const weatherStatus = document.querySelector('#weather-status');
+    const humidityLevel = document.querySelector('#humidity');
+    const windSpeed = document.querySelector('#wind');
+}
