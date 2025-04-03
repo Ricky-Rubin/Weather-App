ffd46dc059887762535d3fee9c357156462e2f5b
Owolabi <owolabi1738@gmail.com>
2025-04-03 09:19:31 +0100

    Wrote the async function to fetch weather data from the web API and display it in the console

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,5 @@
 function weatherApp() {
-    
+
     const searchInput = document.querySelector('#search-input');
     const searchButton = document.querySelector('#search-button');
     const cityName = document.querySelector('#city-name');
@@ -8,4 +8,37 @@ function weatherApp() {
     const weatherStatus = document.querySelector('#weather-status');
     const humidityLevel = document.querySelector('#humidity');
     const windSpeed = document.querySelector('#wind');
+
+    let searchLink = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date]?key=66DFJ3GYA56QMRUNZ83AEG838';
+
+    const theDate = new Date();
+    const currentDate = theDate.toISOString().slice(0, 10);
+    const defaultCity = 'London';
+    const defaultDate = currentDate;
+
+    async function showWeatherInfo() {
+        const city = searchInput.value;
+        const date = currentDate;
+
+        const modifiedLink = searchLink.replace('[location]', city).replace('[date]', date);
+        try {
+            const response = await fetch(modifiedLink, { mode: 'cors'});
+            const mainData = await response.json();
+
+            const fullLocation = mainData.resolvedAddress;
+            const weatherCondition = mainData.currentConditions.conditions;
+            const weatherIcon = mainData.currentConditions.icon;
+            const temperatureValue = mainData.currentConditions.temp;
+            const humidityValue = mainData.currentConditions.humidity;
+            const windValue = mainData.currentConditions.windspeed;
+            
+            console.log(fullocation, weatherCondition, weatherIcon, temperatureValue, humidityValue, windValue);
+        } catch (error) {
+            console.error('It Appears that the city you entered is not valid. Please try again.', error);
+        }
+    }
+
+    showWeatherInfo();
 }
+
+export { weatherApp };
